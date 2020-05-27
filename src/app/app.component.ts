df9dbe45463604d9199dc94e71fa6770ccefe322
madhubn <madhubn.17@gmail.com>
2020-05-27 12:11:43 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl, ValidatorFn } from '@angular/forms';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+form: FormGroup;
+  ordersData = [];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      orders: new FormArray([], minSelectedCheckboxes(10))
+    });
+
+    // async orders
+    of(this.getOrders()).subscribe(orders => {
+      this.ordersData = orders;
+      this.addCheckboxes();
+    });
+
+    // synchronous orders
+    // this.orders = this.getOrders();
+    // this.addCheckboxes();
+  }
+
+  private addCheckboxes() {
+    this.ordersData.forEach((o, i) => {
+      const control = new FormControl(i === 0); // if first item set to true, else false
+      (this.form.controls.orders as FormArray).push(control);
+    });
+  }
+
+  getOrders() {
+    return [
+      { id: 100, name: 'order 1' },
+      { id: 200, name: 'order 2' },
+      { id: 300, name: 'order 3' },
+      { id: 400, name: 'order 4' }
+    ];
+  }
+
+  submit() {
+    const selectedOrderIds = this.form.value.orders
+      .map((v, i) => v ? this.ordersData[i].id : null)
+      .filter(v => v !== null);
+    console.log(selectedOrderIds);
+  }
+}
+
+function minSelectedCheckboxes(min = 10) {
+  const validator: ValidatorFn = (formArray: FormArray) => {
+    const totalSelected = formArray.controls
+      .map(control => control.value)
+      .reduce((prev, next) => next ? prev + next : prev, 0);
+
+    return totalSelected >= min ? null : { required: true };
+  };
+
+  return validator;
 }
